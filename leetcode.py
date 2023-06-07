14aef4d5e892740b87e3fae756278c9e8107cc70
bermet <bermet.alibek@gmail.com>
2023-06-07 05:21:10 -0500

    two-sum problem

---
+++ leetcode.py
@@ -0,0 +1,11 @@
+# Two-sum problem June 6th 2023
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+      seen = {}
+      for i, num in enumerate(nums):
+          complement = target - nums[i]
+          if complement in seen:
+              return [i, seen[complement]]
+          else:
+              seen[num] = i 
