4fbc6f00960976be76adafa3c284cbb9a0456d5c
bermet <bermet.alibek@gmail.com>
2023-06-10 23:15:34 -0500

    solution removed

--- leetcode.py
+++ leetcode.py
@@ -35,14 +35,3 @@
 #         return num + roman[s[-1]]   
 
 # Longest prefix problem
-# class Solution:
-#     # @return a string
-#     def longestCommonPrefix(self, strs):
-#         if not strs:
-#             return ""
-            
-#         for i, letter_group in enumerate(zip(*strs)):
-#             if len(set(letter_group)) > 1:
-#                 return strs[0][:i]
-#         else:
-#             return min(strs)
