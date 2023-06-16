8fe469036119a38f3dd09d5cc617ec25113492c9
bermet <bermet.alibek@gmail.com>
2023-06-15 23:53:08 -0500

    version 2

--- leetcode.py
+++ leetcode.py
@@ -72,3 +72,12 @@
 #             else:
 #                 i = i + 1 
 #         return len(nums) 
+
+# version 2
+
+# x = 1
+# for i in range(len(nums)-1):
+# 	if(nums[i]!=nums[i+1]):
+# 		nums[x] = nums[i+1]
+# 		x+=1
+# return(x)
