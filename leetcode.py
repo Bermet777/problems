8f87bd469b6ec4f10ee464ae47ac4b18a09a0c3c
bermet <bermet.alibek@gmail.com>
2023-06-17 01:54:37 -0500

    remove element

--- leetcode.py
+++ leetcode.py
@@ -84,3 +84,14 @@
 
 #remove element
 
+# class Solution:
+#     def removeElement(self, nums: List[int], val: int) -> int:
+#         x = 0
+      
+#         for i in nums:
+#             if i != val:
+#                 nums[x] = i
+#                 x += 1
+              
+#         return x 
+       
