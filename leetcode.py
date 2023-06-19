5a99900e6ed3372d33f3957d3e331f7733779b41
bermet <bermet.alibek@gmail.com>
2023-06-19 13:50:58 -0500

    index of first ocurrence solution

--- leetcode.py
+++ leetcode.py
@@ -97,3 +97,11 @@
        
 
 # Index of the First Occurrence in a String
+
+# class Solution:
+#     def strStr(self, haystack: str, needle: str) -> int:
+#         if needle not in haystack:
+#             return -1
+#         else:
+#             return len(haystack.split(needle)[0])    
+     
