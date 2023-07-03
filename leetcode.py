9a8d6625598454e5ae91b936def0ac665038d7c5
bermet <bermet.alibek@gmail.com>
2023-07-02 22:38:38 -0500

    u

--- leetcode.py
+++ leetcode.py
@@ -140,14 +140,17 @@
 
 
 # sqrt(x)
-# class Solution(object):
-#     def mySqrt(self, x):
-#         l, r = 0, x
-#         while l <= r:
-#             mid = l + (r-l)//2
-#             if mid * mid <= x < (mid+1)*(mid+1):
-#                 return mid
-#             elif x < mid * mid:
-#                 r = mid - 1
+# def mySqrt(self, x):
+#         lo, hi = 0, x
+        
+#         while lo <= hi:
+#             mid = (lo + hi) // 2
+            
+#             if mid * mid > x:
+#                 hi = mid - 1
+#             elif mid * mid < x:
+#                 lo = mid + 1
 #             else:
-#                 l = mid + 1
+#                 return mid
+        
+    
