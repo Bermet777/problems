461d14f1e1881fe7145abad744a092986bc9223b
bermet <bermet.alibek@gmail.com>
2023-07-03 23:53:48 -0500

    removal

--- leetcode.py
+++ leetcode.py
@@ -139,18 +139,5 @@
 
 
 
-# sqrt(x)
-# def mySqrt(self, x):
-#         lo, hi = 0, x
-        
-#         while lo <= hi:
-#             mid = (lo + hi) // 2
-            
-#             if mid * mid > x:
-#                 hi = mid - 1
-#             elif mid * mid < x:
-#                 lo = mid + 1
-#             else:
-#                 return mid
-        
+
     
