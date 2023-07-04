2ac916e4f900e4a373713ac5080f1a69a7bdfc27
bermet <bermet.alibek@gmail.com>
2023-07-04 03:55:14 -0500

    square root solution

--- leetcode.py
+++ leetcode.py
@@ -138,6 +138,20 @@
 #         return [int(j) for j in str(sum)]
 
 
-
+# sqrt(x)
+# class Solution:
+#     def mySqrt(self, x: int) -> int:
+#         if x == 0:
+#             return 0
+#         low, high = 1, x
+#         while low <= high:
+#             mid = low + (high - low) // 2
+#             if mid*mid == x:
+#                 return mid
+#             elif mid*mid < x:
+#                 low = mid + 1
+#             else:
+#                 high = mid - 1
+#         return high  
 
     
