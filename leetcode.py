e6eafa9bc1bc72fbea16c7e6667bda79ce835b4c
bermet <bermet.alibek@gmail.com>
2023-07-04 23:49:18 -0500

    add binary solution

--- leetcode.py
+++ leetcode.py
@@ -154,4 +154,22 @@
 #                 high = mid - 1
 #         return high  
 
-    
+# add binary
+# class Solution:
+#     def addBinary(self, a: str, b: str) -> str:
+#         carry = 0
+#         result = ''
+
+#         a = list(a)
+#         b = list(b)
+
+#         while a or b or carry:
+#             if a:
+#                 carry += int(a.pop())
+#             if b:
+#                 carry += int(b.pop())
+
+#             result += str(carry %2)
+#             carry //= 2
+
+#         return result[::-1]    
