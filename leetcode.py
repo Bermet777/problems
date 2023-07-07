834f3dbcc9fc162f02834b4d5764969a7c733b51
bermet <bermet.alibek@gmail.com>
2023-07-06 23:35:12 -0500

    merge sorted array

--- leetcode.py
+++ leetcode.py
@@ -184,3 +184,16 @@
 #         for i in range(2, n):
 #             res[i] = res[i-1] + res[i-2]
 #         return res[-1]
+
+#Merge  Sorted Array
+
+# def merge(self, nums1, m, nums2, n):
+#         while m > 0 and n > 0:
+#             if nums1[m-1] >= nums2[n-1]:
+#                 nums1[m+n-1] = nums1[m-1]
+#                 m -= 1
+#             else:
+#                 nums1[m+n-1] = nums2[n-1]
+#                 n -= 1
+#         if n > 0:
+#             nums1[:n] = nums2[:n]
