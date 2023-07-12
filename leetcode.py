f892f3f36cc45a0893502b74a13154d6e6e10f8e
bermet <bermet.alibek@gmail.com>
2023-07-11 23:55:31 -0500

    removal

--- leetcode.py
+++ leetcode.py
@@ -175,15 +175,5 @@
 #         return result[::-1]    
 
 
-# Climbing stairs
 
-# class Solution:
-#     def climbStairs(self, n):
-#         if n == 1:
-#             return 1
-#         res = [0 for i in range(n)]
-#         res[0], res[1] = 1, 2
-#         for i in range(2, n):
-#             res[i] = res[i-1] + res[i-2]
-#         return res[-1]
 
