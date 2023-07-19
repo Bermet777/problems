78d738249b7df1de4f027e7aa7ffabdf63e1cf3d
bermet <bermet.alibek@gmail.com>
2023-07-18 23:57:32 -0500

    climbing stairs solution

--- leetcode.py
+++ leetcode.py
@@ -174,10 +174,14 @@
 
 #         return result[::-1]  
 #
-#
-#
-#
-#
+# Climbing stairs
+# class Solution:
+#     def climbStairs(self, n: int) -> int:
+#         s,l = 1 , 2
+#         for _ in range(2, n): 
+#             # swapping `s` and `l`
+#             s, l = l, s + l 
+#         return l if n > 1 else s
 
 
 
