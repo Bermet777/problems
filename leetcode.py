2bcf22a6ba7e418f022a92d3daf2468bdc8e5e6e
bermet <bermet.alibek@gmail.com>
2023-08-26 23:38:16 -0500

    update

--- leetcode.py
+++ leetcode.py
@@ -197,13 +197,3 @@
 #         if n > 0:
 #             nums1[:n] = nums2[:n]
 
-#Remove duplicates from sorted list
-# class Solution:
-#     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
-#         curr = head
-#         while curr and curr.next:
-#             if curr.val == curr.next.val:
-#                 curr.next = curr.next.next
-#             else:
-#                 curr = curr.next
-#         return head
