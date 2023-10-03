c044ce103781ca3f2af540fb5f36dd64d19f1400
bermet <bermet.alibek@gmail.com>
2023-10-02 23:49:20 -0500

    remove solution

--- leetcode.py
+++ leetcode.py
@@ -120,15 +120,7 @@
 #                 l = mid + 1
 #         return l  
 
-# Length of Last Word
-# class Solution:
-#     def lengthOfLastWord(self, s: str) -> int:
-#         l = s.split()
-   
-#         if l:
-#             return len(l[-1])
-#         return 0    
-                
+
 
 
 
