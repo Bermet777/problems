44e4bb138f81502e0006e5a0cc5447f404583c1f
bermet <bermet.alibek@gmail.com>
2023-10-09 23:57:50 -0500

    update

--- leetcode.py
+++ leetcode.py
@@ -105,20 +105,7 @@
 #         else:
 #             return len(haystack.split(needle)[0])    
      
-# Search Insert Position
-# class Solution:
-#     def searchInsert(self, nums: List[int], target: int) -> int:
-#         l = 0
-#         u = len(nums) - 1
-#         while l <= u:
-#             mid = (l + u) // 2
-#             if nums[mid] == target:
-#                 return mid
-#             elif nums[mid] > target:
-#                 u = mid - 1
-#             else:
-#                 l = mid + 1
-#         return l  
+
 
 
 
