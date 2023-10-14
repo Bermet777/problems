ffb1e81062f86f12df173d51489e0e831769439b
bermet <bermet.alibek@gmail.com>
2023-10-13 23:47:35 -0500

    update

--- leetcode.py
+++ leetcode.py
@@ -62,18 +62,6 @@
 #                 return False    
 #         return stack == []  
 
-# Remove duplicates
-# class Solution:
-#     def removeDuplicates(self, nums: List[int]) -> int:
-#         i = 1
-#         while i < len(nums):
-#             if nums[i] == nums[i-1]:
-#                 nums.pop(i)
-#             else:
-#                 i = i + 1 
-#         return len(nums) 
-
-# version 2 for remove duplicates
 
 
 
