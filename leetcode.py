753e68116ba19c9cfbd509821a8422fed1133d86
bermet <bermet.alibek@gmail.com>
2023-10-19 23:51:13 -0500

    update

--- leetcode.py
+++ leetcode.py
@@ -1,16 +1,5 @@
 # Two-sum problem June 6th 2023
 
-# class Solution:
-#     def twoSum(self, nums: List[int], target: int) -> List[int]:
-#       seen = {}
-#       for i, num in enumerate(nums):
-#           complement = target - nums[i]
-#           if complement in seen:
-#               return [i, seen[complement]]
-#           else:
-#               seen[num] = i 
-
-# Palindrome number problem
 
 
 
